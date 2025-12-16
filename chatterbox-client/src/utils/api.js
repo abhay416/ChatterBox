8cdba5c46a9056170f46e2f7f3cdccdb6e51d20f
Abhay Kumar <abhaykumar96178@gmail.com>
2025-12-17 01:12:05 +0530

    Update api.js

--- chatterbox-client/src/utils/api.js
+++ chatterbox-client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5002/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://chatter-box-g7pn.vercel.app/api",
   headers: { "Content-Type": "application/json" }
 });
 
